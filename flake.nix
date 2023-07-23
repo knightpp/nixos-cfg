4e24a11706bb75f08705dc43ad43faff2b2cb906
Danylo Kondratiev <knightpp@proton.me>
2023-07-23 19:52:25 +0000

    Change hostname

--- flake.nix
+++ flake.nix
@@ -61,7 +61,7 @@
           }));
     in {
       nixosConfigurations = {
-        exampleHost = mkHost "exampleHost" "x86_64-linux";
+        nixboxHost = mkHost "nixbox" "x86_64-linux";
       };
     };
 }
