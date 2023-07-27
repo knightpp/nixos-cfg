422ab7c52b297ac8c76d91e96259085a4a07e04e
Danylo Kondratiev <knightpp@proton.me>
2023-07-27 10:38:00 +0300

    Enable flatpak workaround

--- flake.nix
+++ flake.nix
@@ -65,6 +65,31 @@
               }
             )
 
+            # NOTE: temporary workaround for flatpak
+            (
+              { config, pkgs, ... }: {
+                system.fsPackages = [ pkgs.bindfs ];
+                fileSystems =
+                  let
+                    mkRoSymBind = path: {
+                      device = path;
+                      fsType = "fuse.bindfs";
+                      options = [ "ro" "resolve-symlinks" "x-gvfs-hide" ];
+                    };
+                    aggregated = pkgs.buildEnv {
+                      name = "system-fonts-and-icons";
+                      paths = [ pkgs.libsForQt5.breeze-qt5 ];
+                      pathsToLink = [ "/share/fonts" "/share/icons" ];
+                    };
+                  in
+                  {
+                    # Create an FHS mount to support flatpak host icons/fonts
+                    "/usr/share/icons" = mkRoSymBind "${aggregated}/share/icons";
+                    "/usr/share/fonts" = mkRoSymBind "${aggregated}/share/fonts";
+                  };
+              }
+            )
+
             # Module 6: host
             ./hosts/${hostName}
 
