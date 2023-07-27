57224a871888e803e5d13da5e4371ada542d20f2
Danylo Kondratiev <knightpp@proton.me>
2023-07-27 19:29:43 +0300

    Format filesystems module

--- modules/fileSystems/default.nix
+++ modules/fileSystems/default.nix
@@ -27,39 +27,50 @@ in
       default = [ ];
     };
   };
-  config.fileSystems = mkMerge (mapAttrsToList
-    (dataset: mountpoint: {
-      "${mountpoint}" = {
-        device = "${dataset}";
-        fsType = "zfs";
-        options = [ "X-mount.mkdir" "noatime" ];
-        neededForBoot = true;
-      };
-    })
-    cfg.datasets ++ mapAttrsToList
-    (bindsrc: mountpoint: {
-      "${mountpoint}" = {
-        device = "${bindsrc}";
-        fsType = "none";
-        options = [ "bind" "X-mount.mkdir" "noatime" ];
-      };
-    })
-    cfg.bindmounts ++ map
-    (esp: {
-      "/boot/efis/${esp}" = {
-        device = "${config.zfs-root.boot.devNodes}${esp}";
-        fsType = "vfat";
-        options = [
-          "x-systemd.idle-timeout=1min"
-          "x-systemd.automount"
-          "noauto"
-          "nofail"
-          "noatime"
-          "X-mount.mkdir"
-        ];
-      };
-    })
-    cfg.efiSystemPartitions);
+
+  config.fileSystems = mkMerge (
+    mapAttrsToList
+      (
+        dataset: mountpoint: {
+          "${mountpoint}" = {
+            device = "${dataset}";
+            fsType = "zfs";
+            options = [ "X-mount.mkdir" "noatime" ];
+            neededForBoot = true;
+          };
+        }
+      )
+      cfg.datasets ++
+    mapAttrsToList
+      (
+        bindsrc: mountpoint: {
+          "${mountpoint}" = {
+            device = "${bindsrc}";
+            fsType = "none";
+            options = [ "bind" "X-mount.mkdir" "noatime" ];
+          };
+        }
+      )
+      cfg.bindmounts ++
+    map
+      (
+        esp: {
+          "/boot/efis/${esp}" = {
+            device = "${config.zfs-root.boot.devNodes}${esp}";
+            fsType = "vfat";
+            options = [
+              "x-systemd.idle-timeout=1min"
+              "x-systemd.automount"
+              "noauto"
+              "nofail"
+              "noatime"
+              "X-mount.mkdir"
+            ];
+          };
+        }
+      )
+      cfg.efiSystemPartitions
+  );
   config.swapDevices = mkDefault (map
     (swap: {
       device = "${config.zfs-root.boot.devNodes}${swap}";
