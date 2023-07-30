112ece2f070bce82d5eae5919ed556771939979c
Danylo Kondratiev <knightpp@proton.me>
2023-07-30 21:40:47 +0300

    Add dialout group

--- users/knightpp/default.nix
+++ users/knightpp/default.nix
@@ -7,7 +7,12 @@
       shell = pkgs.fish;
       isNormalUser = true;
       group = "knightpp";
-      extraGroups = [ "nixoscfg" "networkmanager" "systemd-journal" ];
+      extraGroups = [
+        "nixoscfg"
+        "networkmanager"
+        "systemd-journal"
+        "dialout" # for serial ports (esp32)
+      ];
       initialHashedPassword = "$6$pgzhN8I3kJ1O35mZ$dzoVn596Htt3Jc7S1ftGyRnoxHmqvNpY.ZKtN3c/j5y0K3ZlbpwbaMaA6Mw5XnuVQxrDQ0184dkMtZp98thXU1";
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG86t/Sa1mUjJtz7my7fhS0UvK3za5JCOyTw4u58rwvv Personal SSH"
