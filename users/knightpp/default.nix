362adf3758a6798cf354bcc4e8c93e1a821b5c38
Danylo Kondratiev <knightpp@proton.me>
2023-10-30 18:36:23 +0200

    Update helix keybindings

--- users/knightpp/default.nix
+++ users/knightpp/default.nix
@@ -95,6 +95,79 @@
             normal = "block";
             select = "underline";
           };
+          # Colemak DH helix keys
+          keys = {
+            normal = {
+              n = "move_char_left";
+              e = "move_line_down";
+              i = "move_line_up";
+              o = "move_char_right";
+
+              h = "insert_mode";
+              H = "insert_at_line_start";
+
+              l = "open_below";
+              L = "open_above";
+
+              k = "move_next_word_end";
+              K = "move_next_long_word_end";
+
+              j = "search_next";
+              J = "search_prev";
+
+              g = {
+                n = "goto_line_start";
+                o = "goto_line_end";
+              };
+
+              space.w = {
+                n = "jump_view_left";
+                e = "jump_view_down";
+                i = "jump_view_up";
+                o = "jump_view_right";
+              };
+
+              "C-w" = {
+                n = "jump_view_left";
+                e = "jump_view_down";
+                E = "join_selections";
+                "A-E" = "join_selections_space";
+                i = "jump_view_up";
+                I = "keep_selections";
+                "A-I" = "remove_selections";
+                o = "jump_view_right";
+              };
+
+              z = {
+                e = "scroll_down";
+                i = "scroll_up";
+              };
+
+              Z = {
+                e = "scroll_down";
+                i = "scroll_up";
+              };
+            };
+
+            insert = {
+              "A-x" = "normal_mode"; # Maps Alt-X to enter normal mode
+            };
+
+            select = {
+              n = "move_char_left";
+              e = "move_line_down";
+              i = "move_line_up";
+              o = "move_char_right";
+              h = "insert_mode";
+              H = "insert_at_line_start";
+              l = "open_below";
+              L = "open_above";
+              k = "move_next_word_end";
+              K = "move_next_long_word_end";
+              j = "search_next";
+              J = "search_prev";
+            };
+          };
         };
       };
 
