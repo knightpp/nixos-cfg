a13962ecef2c1ec90fb920cdf8e03c1beeed0ec9
Danylo Kondratiev <knightpp@proton.me>
2023-12-02 19:30:31 +0200

    Rewrite nixpkgs-unstable module

--- modules/nixpkgs-unstable/default.nix
+++ modules/nixpkgs-unstable/default.nix
@@ -1,19 +1,28 @@
 {
-  inputs,
+  config,
   lib,
+  inputs,
   pkgs,
   ...
 }: let
+  cfg = config.custom.nixpkgs-unstable;
   optType = lib.mkOptionType {
     name = "nixpkgs";
     description = "An evaluation of Nixpkgs; the top level attribute set of packages";
     check = builtins.isAttrs;
   };
 in {
-  options.pkgs.unstable = lib.mkOption {type = optType;};
+  options = {
+    custom.nixpkgs-unstable = {
+      enable = lib.mkEnableOption "Nixpkgs unstable";
+      pkgs = lib.mkOption {type = optType;};
+    };
+  };
 
-  config.pkgs.unstable = lib.mkDefault (import inputs.unstable {
-    config = {allowUnfree = true;};
-    inherit (pkgs) system;
-  });
+  config = lib.mkIf cfg.enable {
+    custom.nixpkgs-unstable.pkgs = lib.mkDefault (import inputs.unstable {
+      config = {allowUnfree = true;};
+      inherit (pkgs) system;
+    });
+  };
 }
