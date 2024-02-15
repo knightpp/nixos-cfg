63ebb72116884cb82add7eaac0f994bffc2d1506
Danylo Kondratiev <knightpp@proton.me>
2024-02-15 19:27:34 +0200

    Disable prusaslicer

--- modules/desktop-environment/default.nix
+++ modules/desktop-environment/default.nix
@@ -143,7 +143,7 @@ in {
 
     environment.systemPackages = builtins.attrValues {
       superslicer = pkgs.callPackage ./../../pkgs/superslicer.nix {};
-      prusa-slicer = pkgs.callPackage ./../../pkgs/prusaslicer.nix {};
+      # prusa-slicer = pkgs.callPackage ./../../pkgs/prusaslicer.nix {};
 
       inherit
         (pkgs)
