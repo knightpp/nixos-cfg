a744291fb40936978a334d43c70686a5fb5c04f3
Danylo Kondratiev <knightpp@proton.me>
2024-03-19 21:39:11 +0200

    Bring back desktop icon

--- pkgs/superslicer.nix
+++ pkgs/superslicer.nix
@@ -1,13 +1,46 @@
 {
   appimageTools,
   fetchurl,
+  makeDesktopItem,
+  buildEnv,
 }: let
   version = "2.5.59.8";
-in
-  appimageTools.wrapType2 {
+  superslicer = appimageTools.wrapType2 {
     name = "superslicer";
     src = fetchurl {
       url = "https://github.com/supermerill/SuperSlicer/releases/download/${version}/SuperSlicer-ubuntu_18.04-${version}.AppImage";
       hash = "sha256-EEHvdwdSXLvntIZsUfelskA6ywpifO/gVBriu78H3G8=";
     };
+  };
+
+  superslicerDesktopItem = let
+    icon = fetchurl {
+      url = "https://raw.githubusercontent.com/supermerill/SuperSlicer/master/resources/icons/SuperSlicer.svg";
+      hash = "sha256-PhOCUe8FgmTxkqta5uka5YtrWwZl7MiIqYkAIM492X0=";
+    };
+  in
+    makeDesktopItem {
+      name = "SuperSlicer";
+      desktopName = "SuperSlicer";
+      exec = "${superslicer}/bin/superslicer";
+      terminal = false;
+      mimeTypes = [
+        "model/stl"
+        "text/x.gcode"
+        "model/3mf"
+        "application/vnd.ms-3mfdocument"
+        "application/prs.wavefront-obj"
+        "application/x-amf"
+      ];
+      categories = [
+        "Graphics"
+        "3DGraphics"
+        "Engineering"
+      ];
+      inherit icon;
+    };
+in
+  buildEnv {
+    name = "superslicer";
+    paths = [superslicerDesktopItem superslicer];
   }
