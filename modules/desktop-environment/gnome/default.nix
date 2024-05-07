2dc223012771cfb05f7da3d7bc44c163aac2fa44
Danylo Kondratiev <knightpp@proton.me>
2024-05-07 21:48:54 +0300

    Enable 150% scaling in gnome

--- modules/desktop-environment/gnome/default.nix
+++ modules/desktop-environment/gnome/default.nix
@@ -24,7 +24,14 @@ in {
 
     services.xserver.enable = true;
     services.xserver.displayManager.gdm.enable = true;
-    services.xserver.desktopManager.gnome.enable = true;
+    services.xserver.desktopManager.gnome = {
+      enable = true;
+      extraGSettingsOverridePackages = [pkgs.gnome.mutter];
+      extraGSettingsOverrides = ''
+        [org.gnome.mutter]
+        experimental-features=['scale-monitor-framebuffer']
+      '';
+    };
 
     environment.systemPackages = builtins.attrValues {
       inherit
