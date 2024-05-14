b4581c4bd58ebb524fde260a302dcf60aa7a95ee
Danylo Kondratiev <knightpp@proton.me>
2024-05-14 22:58:33 +0300

    Add hyprland

--- modules/desktop-environment/default.nix
+++ modules/desktop-environment/default.nix
@@ -6,7 +6,7 @@
 }: let
   cfg = config.desktop-environment;
 in {
-  imports = [./kde ./gnome];
+  imports = [./kde ./gnome ./hyprland];
 
   options.desktop-environment = {
     enable = lib.mkEnableOption "Desktop Environment";
---
+++ modules/desktop-environment/hyprland/default.nix
@@ -0,0 +1,43 @@
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}: let
+  cfg = config.desktop-environment.hyprland;
+in {
+  options = {
+    desktop-environment.hyprland = {
+      enable = lib.mkEnableOption "Hyprland";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    desktop-environment.enable = true;
+
+    programs.hyprland = {
+      enable = true;
+      package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    };
+
+    # hint electron apps to use wayland:
+    environment.sessionVariables.NIXOS_OZONE_WL = "1";
+    environment.systemPackages = builtins.attrValues {
+      inherit
+        (pkgs)
+        neovim
+        kitty # terminal
+        mako # notification daemon
+        waybar # status bar
+        tofi # launcher minimalistic
+        rofi-wayland # launcher popular
+        ;
+    };
+
+    nix.settings = {
+      substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    };
+  };
+}
