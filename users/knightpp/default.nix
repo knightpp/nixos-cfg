e41d85002c653a4f5c54ae97e0c0e47bd03919c5
Danylo Kondratiev <knightpp@proton.me>
2024-05-30 18:31:57 +0300

    Remove fish as login shell

--- users/knightpp/default.nix
+++ users/knightpp/default.nix
@@ -8,7 +8,6 @@
       initialHashedPassword = "$6$pgzhN8I3kJ1O35mZ$dzoVn596Htt3Jc7S1ftGyRnoxHmqvNpY.ZKtN3c/j5y0K3ZlbpwbaMaA6Mw5XnuVQxrDQ0184dkMtZp98thXU1";
     };
     knightpp = {
-      shell = pkgs.fish;
       isNormalUser = true;
       group = "knightpp";
       extraGroups = [
