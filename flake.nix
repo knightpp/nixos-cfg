353c1bd54330f71c325b2c94c63b74700128cfaf
Danylo Kondratiev <knightpp@proton.me>
2024-06-06 20:41:26 +0300

    Use ref syntax

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-24.05";
+      url = "github:nixos/nixpkgs?ref=nixos-24.05";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager?ref=release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sops-nix = {
@@ -16,7 +16,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-hardware = {
-      url = "github:NixOS/nixos-hardware/master";
+      url = "github:NixOS/nixos-hardware?ref=master";
     };
     # hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
   };
