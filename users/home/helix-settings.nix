ed4c1c3ca144bbc0b85b8a14d05658c7ab13a04e
Danylo Kondratiev <knightpp@proton.me>
2024-06-16 17:48:09 +0300

    Update helix keybindings

--- users/home/helix-settings.nix
+++ users/home/helix-settings.nix
@@ -27,21 +27,22 @@
         i = "goto_line_end";
       };
 
+      t = {
+        t = "match_brackets";
+        s = "surround_add";
+        r = "surround_replace";
+        d = "surround_delete";
+        e = "select_textobject_around";
+        n = "select_textobject_inner";
+      };
+
+      k = "join_selections";
+      K = "join_selections_space";
+
       space.w = {
         m = "jump_view_left";
         n = "jump_view_down";
         e = "jump_view_up";
-        i = "jump_view_right";
-      };
-
-      "C-w" = {
-        m = "jump_view_left";
-        n = "jump_view_down";
-        E = "join_selections";
-        "A-E" = "join_selections_space";
-        e = "jump_view_up";
-        I = "keep_selections";
-        "A-I" = "remove_selections";
         i = "jump_view_right";
       };
 
@@ -57,19 +58,19 @@
     };
 
     select = {
-      m = "move_char_left"; # h
-      n = "move_visual_line_down"; # j
-      e = "move_visual_line_up"; # k
-      i = "move_char_right"; # l
+      m = "extend_char_left"; # h
+      n = "extend_visual_line_down"; # j
+      e = "extend_visual_line_up"; # k
+      i = "extend_char_right"; # l
 
       h = "insert_mode";
       H = "insert_at_line_start";
 
-      f = "move_next_word_end";
-      F = "move_next_long_word_end";
+      f = "extend_next_word_end";
+      F = "extend_next_long_word_end";
 
-      j = "search_next";
-      J = "search_prev";
+      j = "extend_search_next";
+      J = "extend_search_prev";
     };
   };
 }
