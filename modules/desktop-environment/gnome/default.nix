2197dc095e6e0837ab48ac0bf79e77ff986158c7
Danylo Kondratiev <knightpp@proton.me>
2024-11-05 11:21:11 +0200

    Disable gnome suspend

--- modules/desktop-environment/gnome/default.nix
+++ modules/desktop-environment/gnome/default.nix
@@ -33,6 +33,13 @@ in {
       '';
     };
 
+    # disable sleep at all to fix going to sleep when sshed
+    services.xserver.displayManager.gdm.autoSuspend = false;
+    systemd.targets.sleep.enable = false;
+    systemd.targets.suspend.enable = false;
+    systemd.targets.hibernate.enable = false;
+    systemd.targets.hybrid-sleep.enable = false;
+
     # hint electron apps to use wayland:
     environment.sessionVariables.NIXOS_OZONE_WL = "1";
     environment.systemPackages = builtins.attrValues {
